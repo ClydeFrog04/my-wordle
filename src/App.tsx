f17f2cb1a66a6921886d23b1f925de602e52bda8
ClydeFrog04 <egan.randal@gmail.com>
2023-09-04 16:09:28 -0600

    saving work!

--- src/App.tsx
+++ src/App.tsx
@@ -31,9 +31,7 @@ function App() {
                     <Route
                         path={"/"}
                         element={
-                            <div>
-                                <World/>
-                            </div>
+                            <World/>
                         }
                     />
                 </Routes>
